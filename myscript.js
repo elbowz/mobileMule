b118bd03844205490c8032800d6a4e6ee6fd5eb6
muttley <ask@me.it>
2014-05-28 17:28:21 +0200

    Merge "a mano" branch 'master' into donate-pack

--- myscript.js
+++ myscript.js
@@ -39,6 +39,20 @@ $(document).on('pagebeforecreate', function() {
                            </ul>');
 });
 
+$( document ).on( "pagecreate", function() {
+    // Support for open panel menu with swipe
+    $( document ).on( "swipeleft swiperight", "div[data-role='page']", function( e ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
+            if ( e.type === "swiperight" ) {
+                $( "#menu-panel" ).panel( "open" );
+            }
+        }
+    });
+});
+
 $( document ).on("pageinit", "#status", function() {
 	updateStatus();
 	timerStatusUpdate = setInterval(updateStatus, 3000);
@@ -110,7 +124,8 @@ $( document ).on("pageinit", "#first-page", function() {
 	$.mobile.changePage( "amuleweb-main-status.php", { transition: "none" } );	
 });
 
-$(document).on('click','#btScrollUp', function() {
-    console.log('scrollUp');
-    $('body').animate({scrollTop : '0px'}, 'slow', function(){ $('body').clearQueue(); });
+$(document).on('click','#btScrollUp', function(event) {
+    event.preventDefault();
+
+    $('body').animate({scrollTop : '0px'});
 });
