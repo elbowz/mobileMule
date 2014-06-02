67da429a39e23e7b35f265527f21b00cd0a92791
muttley <ask@me.it>
2014-06-02 13:07:16 +0200

    added font awesome (menu panel and other place) version check always visible

--- amuleweb-main-stats-alternative.php
+++ amuleweb-main-stats-alternative.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Statistics - mobileMule</title>
-	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.2/jquery.mobile.min.css" />
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.2/jquery.mobile.min.css" />
 	<link rel="stylesheet" href="mystyle.css" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="myscript.js"></script>
@@ -19,7 +20,7 @@
 
         <div data-role="header" data-position="fixed">
             <a href="#menu-panel" data-rel="close" id="btMenu" data-icon="home" class="ui-btn-left">Menu</a>
-            <h1>Statistics</h1>
+            <h1><i class="fa fa-tachometer fa-fw"></i> Statistics</h1>
             <a href="#" data-rel="back" data-icon="arrow-l" class="ui-btn-right">Back</a>
         </div><!-- /header -->
 
